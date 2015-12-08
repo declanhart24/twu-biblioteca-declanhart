626b895a8416ab81bf210a84382cce35544ca0ca
declanhart24 <declanhart24@gmail.com>
2015-12-08 17:04:49 +1000

    List Books test written

--- TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ExampleTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,7 +32,17 @@ public class ExampleTest {
 
     @Test
     public void testListBooks () {
-        input.
+        String testInput = "1\n1\n";
+//        testInput.toByteArray
+        String expectedOutput =
+        "1 Head First Java       Kathy Sierra & Bert Bates                2005\n\n" +
+        "Please Select from the following options\n\n" +
+        "1 List Books\n" +
+        "2 Check out a book\n" +
+        "3 Return a book\n" +
+        "5 Exit\n\n" +
+        "Please enter the corresponding menu item number:";
+        assertEquals(expectedOutput, system.menu());
     }
 
 }
